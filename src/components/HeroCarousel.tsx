ab404071c9a0ea154ef6fe8c5f59e2cf2e866e62
Ananthakrishnan A N <125691030+ananthucigil@users.noreply.github.com>
2025-07-18 18:28:18 +0530

    Position Div at Bottom of Carousel

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -246,7 +246,7 @@ const HeroCarousel = () => {
       </button>
 
       {/* Slide Indicators - Mobile optimized */}
-      <div className="absolute bottom-16 sm:bottom-20 lg:bottom-24 left-1/2 transform -translate-x-1/2 flex space-x-2 sm:space-x-3">
+      <div className="absolute bottom-4 sm:bottom-6 lg:bottom-8 left-1/2 transform -translate-x-1/2 flex space-x-2 sm:space-x-3">
         {slides.map((_, index) => (
           <button
             key={index}
